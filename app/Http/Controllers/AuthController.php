ee0961c0a84e6e758332515894ef834b10d80bbb
Tuhin Rana Mondal <aimlesstuhin@gmail.com>
2025-06-28 23:03:46 +0530

    First Commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $user = User::where('email',$request->email)->first();
+        if(is_null($user)){
+            return response()->json([
+                'message'=>'user not Found!'
+            ]);
+        }
+        if(! password_verify($request->password, $user->password)){
+            return response()->json([
+                'token' => 'Password not Matched!'
+            ]);
+        }
+        $token = $user->createToken('api')->plainTextToken;
+        return response()->json([
+            'token' => $token
+        ]);
+    }
+
+    public function register(RegisterRequest $request): JsonResponse
+    {
+
+        dd($request);
+        return response()->json([
+            'message'=>'Successfully Registered!'
+        ]);
+    }
+}
